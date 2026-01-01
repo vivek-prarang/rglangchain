80032979e0d1b8d6aeb4dcfe4ab657bb46a6232c
ASG Kali new <vivek.k@prarang.in>
2026-01-01 18:20:11 +0530

    old

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,18 +5,18 @@ class Settings(BaseSettings):
     app_name: Optional[str] = "FastAPI"
     debug: Optional[bool] = False
     app_version: Optional[str] = "0.0.1"
-    
+
     app_port : Optional[int] = 8000
     app_host : Optional[str] = "127.0.0.1"
-    
+
     database_url: Optional[str] = None
     secret_key: Optional[str] = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     algorithm: Optional[str] = "HS256"
     access_token_expire_min: Optional[int] = 30
-    
-    
+
+
     class Config:
         env_file = ".env"
         case_sensitive = False
-        extra = "allow" 
+        extra = "allow"
 settings=Settings()
--- app/models/user_model.py
+++
@@ -1,2 +0,0 @@
-# from datetime import datetime
-# from sqlalchemy import Column, Integer, String, DateTime
